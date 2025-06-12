eb66712ea9235f09f07ae0b77ac9e979ca6c9a02
Adamfinexia <adam.finexia@icloud.com>
2025-06-12 19:06:08 +0200

    Add files via upload

---
+++ pages/index.js
@@ -0,0 +1,9 @@
+
+export default function Home() {
+  return (
+    <div style={{ padding: '2rem', fontFamily: 'Arial' }}>
+      <h1>Finexia działa!</h1>
+      <p>Twoja aplikacja budowlana jest gotowa do instalacji jako PWA.</p>
+    </div>
+  );
+}
