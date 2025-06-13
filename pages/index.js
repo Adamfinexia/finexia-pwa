8f7ea79747d14d517ea285c19e81d49d8b0f6dff
Adamfinexia <adam.finexia@icloud.com>
2025-06-13 09:01:01 +0200

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -2,7 +2,7 @@
 export default function Home() {
   return (
     <div style={{ padding: '2rem', fontFamily: 'Arial' }}>
-      <h1>Finexia działa! 🚀</h1>
+      <h1>Finexia działa! 🚧 (v2)</h1>
       <p>Twoja aplikacja budowlana jest gotowa do instalacji jako PWA.</p>
     </div>
   );
