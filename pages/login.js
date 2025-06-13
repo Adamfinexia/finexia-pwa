fee45334da51a2528c11ac4d5ed07f7f170d6ab5
Adamfinexia <adam.finexia@icloud.com>
2025-06-13 14:03:43 +0200

    Add files via upload

--- pages/login.js
+++ pages/login.js
@@ -1,6 +1,6 @@
 import { useState } from 'react';
 import { signInWithEmailAndPassword } from 'firebase/auth';
-import { auth } from '../firebaseConfig.js'
+import { auth } from '../firebaseConfig';
 import { useRouter } from 'next/router';
 
 export default function LoginPage() {
